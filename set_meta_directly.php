1b9c0fc28ea9835d10a882c56d6c68712e43f0b9
Iluminaciones Garcia <soporte@iluminacioneseyg.com>
2026-07-25 18:57:45 -0500

    SEO: Remove temporary direct postmeta updater

--- set_meta_directly.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('wp-load.php');
-update_post_meta(4179, '_yoast_wpseo_title', 'Iluminaciones García | Artículos de Iluminación y Soluciones Eléctricas');
-update_post_meta(4179, '_yoast_wpseo_metadesc', 'Encuentra en Iluminaciones García la mejor selección de artículos de iluminación profesional y soluciones eléctricas en el Perú. ¡Cotiza hoy por WhatsApp!');
-clean_post_cache(4179);
-echo "Updated successfully!\n";
