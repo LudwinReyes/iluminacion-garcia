9d2aae752fe6b2f0bf397488104225d5eb3c12a7
Iluminaciones Garcia <soporte@iluminacioneseyg.com>
2026-07-26 12:36:34 -0500

    SEO: Update REST response checker with admin user

--- check_rest_response.php
+++ check_rest_response.php
@@ -1,5 +1,14 @@
 <?php
 require_once('wp-load.php');
+$admins = get_users(['role' => 'administrator']);
+if (!empty($admins)) {
+    $admin = $admins[0];
+    wp_set_current_user($admin->ID);
+    echo "Logged in as administrator: " . $admin->user_login . "\n";
+} else {
+    echo "No administrator found.\n";
+}
+
 $request = new WP_REST_Request('GET', '/wc/gla/mc/contact-information');
 $response = rest_do_request($request);
 print_r($response->get_data());
