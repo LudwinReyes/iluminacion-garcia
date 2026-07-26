c121d459ae79140674251e14620dae67d5d717c8
Iluminaciones Garcia <soporte@iluminacioneseyg.com>
2026-07-25 19:02:24 -0500

    SEO: Update reflection script for container keys

--- test_container.php
+++ test_container.php
@@ -1,7 +1,6 @@
 <?php
 require_once('wp-load.php');
 $reflection = new ReflectionClass('Yoast\WP\Lib\Dependency_Injection\Container_Registry');
-$method = $reflection->getMethod('get');
-foreach ($method->getParameters() as $param) {
-    echo $param->getName() . "\n";
-}
+$prop = $reflection->getProperty('containers');
+$prop->setAccessible(true);
+print_r(array_keys($prop->getValue()));
