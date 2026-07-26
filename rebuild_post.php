2cbfa2c85b1066ef5ce154a99f7fe7655bf5a46f
Iluminaciones Garcia <soporte@iluminacioneseyg.com>
2026-07-25 19:02:04 -0500

    SEO: Update Yoast rebuilder with 'wpseo' container ID

--- rebuild_post.php
+++ rebuild_post.php
@@ -1,8 +1,8 @@
 <?php
 require_once('wp-load.php');
 if (class_exists('Yoast\WP\Lib\Dependency_Injection\Container_Registry')) {
-    $repository = Yoast\WP\Lib\Dependency_Injection\Container_Registry::get('Yoast\WP\SEO\Repositories\Indexable_Repository');
-    $builder = Yoast\WP\Lib\Dependency_Injection\Container_Registry::get('Yoast\WP\SEO\Builders\Indexable_Builder');
+    $repository = Yoast\WP\Lib\Dependency_Injection\Container_Registry::get('Yoast\WP\SEO\Repositories\Indexable_Repository', 'wpseo');
+    $builder = Yoast\WP\Lib\Dependency_Injection\Container_Registry::get('Yoast\WP\SEO\Builders\Indexable_Builder', 'wpseo');
     
     $indexable = $repository->find_by_id_and_type(4179, 'post');
     if ($indexable) {
