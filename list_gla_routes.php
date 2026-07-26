2a5e0ea445e72f1972a1eed5e0d3d32842060527
Iluminaciones Garcia <soporte@iluminacioneseyg.com>
2026-07-26 12:34:54 -0500

    SEO: Update route lister search term

--- list_gla_routes.php
+++ list_gla_routes.php
@@ -3,7 +3,7 @@ require_once('wp-load.php');
 $server = rest_get_server();
 $routes = array_keys($server->get_routes());
 foreach ($routes as $route) {
-    if (stripos($route, 'google') !== false) {
+    if (stripos($route, 'gla') !== false) {
         echo $route . "\n";
     }
 }
