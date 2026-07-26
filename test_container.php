a434521958705aec1b6477324a9f23480c40a8c5
Iluminaciones Garcia <soporte@iluminacioneseyg.com>
2026-07-25 19:01:43 -0500

    SEO: Update reflection script for parameters

--- test_container.php
+++ test_container.php
@@ -1,6 +1,7 @@
 <?php
 require_once('wp-load.php');
 $reflection = new ReflectionClass('Yoast\WP\Lib\Dependency_Injection\Container_Registry');
-foreach ($reflection->getMethods() as $method) {
-    echo $method->getName() . "\n";
+$method = $reflection->getMethod('get');
+foreach ($method->getParameters() as $param) {
+    echo $param->getName() . "\n";
 }
