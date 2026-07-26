42bcccb2ccc253236e4ac363874af419c46fb871
Iluminaciones Garcia <soporte@iluminacioneseyg.com>
2026-07-26 12:35:45 -0500

    SEO: Update REST response checker path

--- check_rest_response.php
+++ check_rest_response.php
@@ -1,5 +1,5 @@
 <?php
 require_once('wp-load.php');
-$request = new WP_REST_Request('GET', '/google-listings-and-ads/v1/merchant/contact-information');
+$request = new WP_REST_Request('GET', '/wc/gla/mc/contact-information');
 $response = rest_do_request($request);
 print_r($response->get_data());
