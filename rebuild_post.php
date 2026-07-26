873d4164d67eb81a077369d34ec974a7b4f9db29
Iluminaciones Garcia <soporte@iluminacioneseyg.com>
2026-07-25 19:01:23 -0500

    SEO: Rebuild homepage indexable using Container_Registry

--- rebuild_post.php
+++ rebuild_post.php
@@ -1,8 +1,8 @@
 <?php
 require_once('wp-load.php');
-if (function_exists('yoast_get_by_classname')) {
-    $repository = yoast_get_by_classname('Yoast\WP\SEO\Repositories\Indexable_Repository');
-    $builder = yoast_get_by_classname('Yoast\WP\SEO\Builders\Indexable_Builder');
+if (class_exists('Yoast\WP\Lib\Dependency_Injection\Container_Registry')) {
+    $repository = Yoast\WP\Lib\Dependency_Injection\Container_Registry::get('Yoast\WP\SEO\Repositories\Indexable_Repository');
+    $builder = Yoast\WP\Lib\Dependency_Injection\Container_Registry::get('Yoast\WP\SEO\Builders\Indexable_Builder');
     
     $indexable = $repository->find_by_id_and_type(4179, 'post');
     if ($indexable) {
@@ -11,7 +11,7 @@ if (function_exists('yoast_get_by_classname')) {
     
     $indexable = $repository->find_by_id_and_type(4179, 'post');
     $builder->build($indexable);
-    echo "Indexable for post 4179 rebuilt successfully via global helper!\n";
+    echo "Indexable for post 4179 rebuilt successfully using Container_Registry static get!\n";
 } else {
-    echo "yoast_get_by_classname function not found.\n";
+    echo "Container_Registry class not found.\n";
 }
