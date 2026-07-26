94a9d6bbeeb51fa5fbdbe7122865c58bc3bf2390
Iluminaciones Garcia <soporte@iluminacioneseyg.com>
2026-07-25 19:03:05 -0500

    SEO: Swap Yoast Container_Registry arguments

--- rebuild_post.php
+++ rebuild_post.php
@@ -1,8 +1,8 @@
 <?php
 require_once('wp-load.php');
 if (class_exists('Yoast\WP\Lib\Dependency_Injection\Container_Registry')) {
-    $repository = Yoast\WP\Lib\Dependency_Injection\Container_Registry::get('Yoast\WP\SEO\Repositories\Indexable_Repository', 'yoast-seo');
-    $builder = Yoast\WP\Lib\Dependency_Injection\Container_Registry::get('Yoast\WP\SEO\Builders\Indexable_Builder', 'yoast-seo');
+    $repository = Yoast\WP\Lib\Dependency_Injection\Container_Registry::get('yoast-seo', 'Yoast\WP\SEO\Repositories\Indexable_Repository');
+    $builder = Yoast\WP\Lib\Dependency_Injection\Container_Registry::get('yoast-seo', 'Yoast\WP\SEO\Builders\Indexable_Builder');
     
     $indexable = $repository->find_by_id_and_type(4179, 'post');
     if ($indexable) {
@@ -11,7 +11,7 @@ if (class_exists('Yoast\WP\Lib\Dependency_Injection\Container_Registry')) {
     
     $indexable = $repository->find_by_id_and_type(4179, 'post');
     $builder->build($indexable);
-    echo "Indexable for post 4179 rebuilt successfully using Container_Registry static get!\n";
+    echo "Indexable for post 4179 rebuilt successfully!\n";
 } else {
     echo "Container_Registry class not found.\n";
 }
