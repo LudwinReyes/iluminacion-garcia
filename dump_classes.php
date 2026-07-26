0663b8de5ec5ecd4a0c067c844830b9093049244
Iluminaciones Garcia <soporte@iluminacioneseyg.com>
2026-07-25 19:03:28 -0500

    SEO: Remove temporary debugging files

--- dump_classes.php
+++
@@ -1,8 +0,0 @@
-<?php
-$yoast_classes = [];
-foreach (get_declared_classes() as $class) {
-    if (stripos($class, 'Yoast') !== false || stripos($class, 'WPSEO') !== false) {
-        $yoast_classes[] = $class;
-    }
-}
-print_r(array_slice($yoast_classes, 0, 50));
--- test_container.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('wp-load.php');
-$reflection = new ReflectionClass('Yoast\WP\Lib\Dependency_Injection\Container_Registry');
-$prop = $reflection->getProperty('containers');
-$prop->setAccessible(true);
-print_r(array_keys($prop->getValue()));
